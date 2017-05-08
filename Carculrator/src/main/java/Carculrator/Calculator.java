857a0aca3ecc8647b441a732c33cb5d84c3cd5bd
TK <xorrudwkd@gmail.com>
2017-05-09 00:30:52 +0900

    계산기

---
+++ Carculrator/src/main/java/Carculrator/Calculator.java
@@ -0,0 +1,27 @@
+package Carculrator;
+
+public class Calculator {
+
+	private int r=0;
+	public Calculator(int i) {
+		r=i;
+	}
+
+	public Calculator() {
+	}
+
+	public int getResult() {
+			return r;
+	}
+
+	public void add(int i) {
+		r+=i;
+		
+	}
+
+	public void clear() {
+		
+		
+	}
+
+}
